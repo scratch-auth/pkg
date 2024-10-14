93cdffb04d7cb99a88957d54be98657ffdca063c
Fun117 <fun117.kun+dev@gmail.com>
2024-10-14 17:28:04 +0900

    セキュリティーアップデート
    - 暗号化方法を AES-GCM に変更
    - CSS の更新

---
+++ test/nextjs/src/app/api/auth/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { setScratchAuthSession } from "@scratch-auth/nextjs";
+
+export default function AuthPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const privateCode = searchParams.get("privateCode");
+
+  useEffect(() => {
+    async function auth() {
+      console.log(privateCode);
+      const check = await setScratchAuthSession(privateCode);
+      console.log("check", check);
+      if (check) {
+        console.log("認証に成功しました");
+      } else {
+        console.error("認証に失敗しました");
+      }
+      router.push("/");
+    }
+    auth();
+  }, [privateCode]);
+
+  return (
+    <div className="flex justify-center items-center w-full h-full min-h-[calc(100dvh-64px)]">
+      Scratchアカウントを認証中...
+    </div>
+  );
+}
